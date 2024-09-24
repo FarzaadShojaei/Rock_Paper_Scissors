f7317469c0dce8bf0f594c166465200406907331
farzaadshojaei <farzaadshojaei@gmail.com>
2024-09-24 21:40:23 +0330

    Initial Commit

---
+++ app/src/main/java/ir/sample/rock_paper_scissors/RockPaperScissors.kt
@@ -0,0 +1,27 @@
+package ir.sample.rock_paper_scissors
+
+fun main(){
+
+    var computerChoice=""
+    var playerChoice=""
+
+    println("Rock, Paper or Scissors? Enter your choice!")
+    playerChoice= readln()
+
+
+   val randomNumber = (1..3).random();
+
+    if(randomNumber ==1){
+        computerChoice= "Rock"
+    }
+    else if(randomNumber ==2){
+        computerChoice= "Paper"
+    }
+
+   else if(randomNumber == 3){
+        computerChoice="Scissors"
+    }
+    println(computerChoice)
+
+
+}
